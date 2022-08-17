acc425085435daf9a8795f964b2eabb27d59e8ea
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-08-17 23:15:16 +0200

    Initial commit

---
+++ src/com/tilmeez/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package springdemo;
+
+public class BaseballCoach {
+
+    public String getDailyWorkout() {
+        return "30 min on batting practice";
+    }
+}
+
---
+++ src/com/tilmeez/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package com.tilmeez.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        // retrieve beans from spring container
+           Coach theCoach = context.getBean("myCoach", Coach.class);
+
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        // check if they are same
+        boolean result = (theCoach == alphaCoach);
+
+        // print out result
+        System.out.println("pointing to the same object: " + result);
+
+        System.out.println("Memory location fot theCoach: " + theCoach);
+
+        System.out.println("Memory location fot alphaCoach: " + alphaCoach);
+
+        // close the context
+        context.close();
+    }
+}
